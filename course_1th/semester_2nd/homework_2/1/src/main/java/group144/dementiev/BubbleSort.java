ab20367f9fa4717f1714a4174248a12d45bc1a0f
Alex Dementiev <alex.dementiev.work@gmail.com>
2018-05-01 19:15:10 +0300

    init

---
+++ course_1th/semester_2nd/homework_2/1/src/main/java/group144/dementiev/BubbleSort.java
@@ -0,0 +1,14 @@
+package group144.dementiev;
+
+public class BubbleSort implements Sorter {
+    @Override
+    public void sort(int[] array) {
+        for (int i = 0; i < array.length; i++)
+            for (int j = 0; j < array.length - 1; j++)
+                if (array[j] < array[j+1]) {
+                    int tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                }
+    }
+}
